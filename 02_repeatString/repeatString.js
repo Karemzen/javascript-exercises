19953bcbc99ca78b19fa1071d21831c309d0e310
“Karemzen” <zenzapper@hotmail.com>
2024-03-21 11:29:45 +0000

    first 2 exercises done, repeat strings and hello world

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(text, times) {
 
+    if (times < 0) {
+        return "ERROR";
+    }
+    let empty = "";
+    for (let i =0; i < times; i++) {
+        empty = empty + text;
+    }
+    return empty;
+    
 };
 
+repeatString("hey", -1)
 // Do not edit below this line
 module.exports = repeatString;
