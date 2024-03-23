ce5a06c6849dc590a4ac7f67242ca876e7d67456
“Karemzen” <zenzapper@hotmail.com>
2024-03-23 11:52:56 +0000

    reverse string 03 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,27 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    const arrayOfLetters = [];
+    for (let i = 0; i < string.length; i++) {
+        // console.log(string[i]);
+        arrayOfLetters.push(string[i]);
+    }
+
+    let reversedString = "";
+    const reversedArray = arrayOfLetters.reverse();
+
+    for (let i = 0; i < arrayOfLetters.length; i++) {
+        reversedString += reversedArray[i];
+    }
+    
+    
+    console.log(reversedString);
+    return reversedString;
 };
+
+reverseString("hola me llamo es karem");
+//get each letter seperately
+//put each letter in an array
+//turn array into string
 
 // Do not edit below this line
 module.exports = reverseString;
