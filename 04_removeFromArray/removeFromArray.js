cad1ea8a6f09d9ca95de0013db7760e967268bca
“Karemzen” <zenzapper@hotmail.com>
2024-03-27 10:02:16 +0000

    sum all completed, number 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,26 @@
-const removeFromArray = function() {
+var removeFromArray = function(array, ...args) {
 
-};
+    
+    const bob = array.filter(element => !args.includes(element))
+    console.log(bob)
+    return bob;
+
+    const isNotNumber = array.filter(element => element != args)
+
+    console.log(isNotNumber);
+    return isNotNumber;
+//     for (i = 0; i < array.length-1; i++) {
+        
+//         if (array[i].equals(toRemove) ) {
+//             array.splice(i, 1)
+//         }
+
+        
+//     }
+
+ };
+
+ removeFromArray([1,2,3,4], 1, 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,39 @@
-const sumAll = function() {
+const sumAll = function(val1, val2) {
 
+    let biggestNum;
+    let smallestNum;
+
+    if (typeof val1 != "number" || typeof val2 != "number") {
+        return "ERROR";
+    } 
+    else if (val1 < 0 || val2 < 0) {
+        return "ERROR";
+    }
+    else if (val1 > val2) {
+         biggestNum = val1;
+         smallestNum = val2;
+    } else {
+        biggestNum = val2;
+        smallestNum = val1;
+    }
+    const arrayToSum = [biggestNum];
+    let sum = 0;
+    //get all numbers and put into an array
+    //add up array
+
+    for (let i = biggestNum; i > smallestNum; i--) {
+        arrayToSum.push(i-1);
+    }
+    console.log(arrayToSum);
+
+    for (let i = 0; i < arrayToSum.length; i++) {
+        sum += arrayToSum[i];
+    }
+    return sum;
 };
 
+
+
+sumAll(1,4)
 // Do not edit below this line
 module.exports = sumAll;
