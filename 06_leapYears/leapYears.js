d22bc17740088b8f7be129dc605adf487f6e59a8
“Karemzen” <zenzapper@hotmail.com>
2024-03-29 09:52:27 +0000

    leap years (6) and temp conversions (07)

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,23 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if (year % 100 === 0) { //years divisible by 100 arent leap years
+        if (year % 400 ===0) {
+            return true
+        }
+        return false
+    }
+
+    
+
+    else if (year % 4 === 0) { //leap years are divisible by four
+        return true
+    }
+     
+
+    else return false
+    
 };
 
+leapYears(2000);
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitInput) {
+
+  let celsiusConversion = (farenheitInput - 32) * 5/9;
+  return Math.round(celsiusConversion * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusInput) {
+  let farnheitConversion = (celsiusInput * 9/5) + 32;
+  return Math.round(farnheitConversion * 10) / 10;
 };
 
 // Do not edit below this line
